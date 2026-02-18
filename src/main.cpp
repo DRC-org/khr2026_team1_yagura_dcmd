1efb1204d44d79fc790d519fe5451117a6055a1e
Taiga Saito <me@taigasaito.org>
2026-02-18 15:56:33 +0900

    feat: yagura_dcmd

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,6 @@
 #include <Arduino.h>
+#include <Arduino_CAN.h>
 #include <FastLED_NeoPixel.h>
-#include <SPI.h>
-#include <mcp_can.h>
 #include <pwm.h>
 
 void readSwitch();
@@ -15,12 +14,13 @@ void motorDrive(int direction);
 #define ENC_A 2
 #define ENC_B 3
 #define ENC_X 15
-#define SW_A 16
-#define SW_B 17
+#define SW_A 16  // リミット SW 上
+#define SW_B 17  // リミット SW 下
 #define SENS 14
 #define MCP_INT 12
 // 通信関係
-#define CAN_CS 5
+#define CAN_RX 5
+#define CAN_TX 4
 #define SCL 19
 #define SDA 18
 // LED 関係
@@ -40,8 +40,6 @@ PwmOut pwm_n(PWM_N);
 PwmOut pwm_l(PWM_L);
 
 FastLED_NeoPixel<1, RGB, NEO_GRB> strip;  // RGBLED 制御用
-
-MCP_CAN CAN(CAN_CS);  // MCP2515 CS ピン指定
 
 unsigned int CAN_ID;  // CANのID（DC モータドライバは 0x300 番台）
 int motorState = 0;   // モータの状態（正転: 1、逆転: -1、停止: 0）
@@ -74,7 +72,7 @@ void setup() {
   pwm_n.begin((uint32_t)30, (uint32_t)0);  // スイッチング周期 30 µs（≒ 33 kHz）
   pwm_l.begin((uint32_t)30, (uint32_t)0);  // スイッチング周期 30 µs（≒ 33 kHz）
 
-  if (CAN.begin(MCP_ANY, CAN_1000KBPS, MCP_16MHZ) != CAN_OK) {
+  if (!CAN.begin(CanBitRate::BR_1000k)) {
     Serial.println("CAN.begin(...) failed.");
     while (1) {
       strip.setPixelColor(0, strip.Color(255, 0, 0));
@@ -85,21 +83,17 @@ void setup() {
       delay(100);
     }
   }
-  CAN.setMode(MCP_NORMAL);
 
   readSwitch();  // DIP スイッチの読み出して CAN_ID を指定
 }
 
 void loop() {
-  if (CAN.checkReceive() == CAN_MSGAVAIL) {
-    unsigned long rxId;
-    unsigned char len;
-    unsigned char buf[8];
-    CAN.readMsgBuf(&rxId, &len, buf);  // 受信した CAN の読み出し
+  if (CAN.available()) {
+    CanMsg const msg = CAN.read();  // 受信した CAN の読み出し
+    unsigned int rxId = msg.id;
 
     if (rxId == CAN_ID) {
-      unsigned char command = buf[0];
-
+      unsigned char command = msg.data[0];
       if (command == 0x00) {  // 正転
         motorState = 1;
       } else if (command == 0x01) {  // 逆転
@@ -108,14 +102,16 @@ void loop() {
         motorState = 0;
       }
     }
-    Serial.print("motorState : ");
-    Serial.println(motorState);
+  }
+
+  if (motorState == 1 && digitalRead(SW_A) == LOW) {  // 上まで達したとき
+    motorState = 0;
+  } else if (motorState == -1 &&
+             digitalRead(SW_B) == LOW) {  // 下まで達したとき
+    motorState = 0;
   }
 
   motorDrive(motorState);
-
-  Serial.print(digitalRead(SW_A));
-  Serial.println(digitalRead(SW_B));
 }
 
 // DIPスイッチを読み取ってCAN_IDを指定する
